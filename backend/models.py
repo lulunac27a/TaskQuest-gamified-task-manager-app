4756afc8323ed05228719706d25009fdb6721791
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2026-06-01 18:25:34 -0500

    Fix error for user model

--- backend/models.py
+++ backend/models.py
@@ -4,16 +4,24 @@ from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from extensions import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):  # user model with XP and level
     id = db.Column(db.Integer, primary_key=True)  # user id
     username = db.Column(db.String(80), unique=True,
                          nullable=False)  # username
+    password_hash = db.Column(db.String(128), nullable=False)  # password hash
     xp = db.Column(db.Integer, default=0)  # user XP
     level = db.Column(db.Integer, default=1)  # user level
     tasks = db.relationship("Task", backref="user",
                             lazy=True)  # user tasks list
+
+    def set_password(self, password):  # function to set user password
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):  # function to check user password
+        return check_password_hash(self.password_hash, password)
 
     def add_xp(self, amount):  # function to add XP and level up if necessary
         self.xp += amount  # add XP to user
