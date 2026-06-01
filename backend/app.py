f47fc3de375da5ff69f57737257cc96c59905b96
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2026-06-01 18:09:14 -0500

    Add werkzeug package to imports

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 from models import db, User, Task
 from routes.tasks import tasks_bp
 
