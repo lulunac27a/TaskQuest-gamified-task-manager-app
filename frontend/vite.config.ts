79ae6312337d61dbca659da4e074ff9abf5df67f
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2026-06-01 17:07:37 -0500

    Format TypeScript code

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:5000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
-})
+    plugins: [vue()],
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://127.0.0.1:5000",
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
+});
